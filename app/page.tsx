0b17c41b7ab1714795c07921999febc9d4698793
Mayank Arya <mmaismma@hotmail.com>
2025-03-30 20:35:18 +0530

    Refactor homepage; add random greeting & integrate `ActionList`

--- app/page.tsx
+++ app/page.tsx
@@ -1,36 +1,62 @@
-import Link from "next/link";
-import Image from "next/image";
-import "./page.module.css"
-const HomePage = () => {
-  return (
-    <div className="HomePage-Cnt">
-      <div className="logo-Cnt">
-        <Image src="/mascot.webp" alt="logo" className="logo-image" width={190} height={301} />
-      </div>
-      <div>
-        <h2 className="random-Greeting">random greeting</h2>
-      </div>
-      <div>
-        <h1 className="title">Unira</h1>
-      </div>
-      <div>
-        <h3 className="about">¡
-          Step into the world where <br />
-          conversations spark
-          <br />
-          connections!
-        </h3>
-      </div>
-      <div>
-        <Link href="get-started">Get Started</Link>
-      </div>
-      <div>
-        <a>
-          <h3 className="explore">or explore first!</h3>
-        </a>
-      </div>
-    </div>
-  );
-};
+"use client";
 
-export default HomePage;
+import Image from "next/image";
+import ActionList from "@/_components/action-list";
+import styles from "./page.module.css";
+import { useState } from "react";
+
+const greetings = [
+    // "welcome to a new heaven",
+    // "where shadows meet, or not",
+    // "the veil parts, meet",
+    // "a place found, enter",
+    // "prepare for awesome",
+    // "enter the fun zone!",
+    // "ready to play? Enter",
+    // "your quest starts here!",
+    // "press start to continue...",
+    "here be fun",
+    // "you made it to wonderland!",
+    // "hello adventurer, this is",
+    // "hola amigo! kaise ho?",
+    // "fingerlicking delicious",
+];
+
+export default function Home() {
+    const [randomGreeting] = useState(() => greetings[Math.floor(Math.random() * greetings.length)]);
+    // const randomGreeting = greetings[Math.floor(Math.random() * greetings.length)];
+
+    return (
+        <main className={styles.main}>
+            <section>
+                <Image
+                    src="/mascot.webp"
+                    alt="Cartoon of a happy student"
+                    width={190}
+                    height={301}
+                    className={styles.mascot}
+                    priority
+                />
+                <h2>{randomGreeting}</h2>
+                <h1>Unira</h1>
+                <p>Step into the world where conversations spark connections!</p>
+            </section>
+
+            <section style={{ fontSize: "1.3rem" }}>
+                <ActionList
+                    actions={[
+                        {
+                            label: <span>Get Started</span>,
+                            href: "/login",
+                            primary: true,
+                        },
+                        {
+                            label: <span>explore first!</span>,
+                            href: "/explore",
+                        },
+                    ]}
+                />
+            </section>
+        </main>
+    );
+}
