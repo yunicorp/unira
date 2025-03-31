b52e66cdf8a0889a6b1f9c908229e97b35a43b7e
Mayank Arya <mmaismma@hotmail.com>
2025-04-01 02:25:46 +0530

    Remove random greeting

--- app/page.tsx
+++ app/page.tsx
@@ -3,28 +3,8 @@
 import Image from "next/image";
 import ActionList from "@/_components/action-list";
 import styles from "./page.module.css";
-import { useState } from "react";
-
-const greetings = [
-    // "welcome to a new heaven",
-    // "where shadows meet, or not",
-    // "the veil parts, meet",
-    // "a place found, enter",
-    // "prepare for awesome",
-    // "enter the fun zone!",
-    // "ready to play? Enter",
-    // "your quest starts here!",
-    // "press start to continue...",
-    "here be fun",
-    // "you made it to wonderland!",
-    // "hello adventurer, this is",
-    // "hola amigo! kaise ho?",
-    // "fingerlicking delicious",
-];
 
 export default function Home() {
-    const [randomGreeting] = useState(() => greetings[Math.floor(Math.random() * greetings.length)]);
-
     return (
         <main className={styles.main}>
             <section className={styles.header}>
@@ -36,7 +16,6 @@ export default function Home() {
                     height={301}
                     priority
                 />
-                <h2>{randomGreeting}</h2>
                 <h1>Unira</h1>
                 <p>Step into the world where conversations spark connections!</p>
             </section>
