85329234cbbaf13fc66b3b60efb76203e8bf5233
Techkulkul <079singhkulkul@gmail.com>
2025-08-01 01:09:22 +0530

    Refactor BottomNav component to update links and improve layout styling

--- app/_components/buttonNav/index.tsx
+++ app/_components/buttonNav/index.tsx
@@ -6,28 +6,28 @@ import { Home, Search, MessageSquare, User } from "lucide-react";
 import "./style.css";
 
 const BottomNav: React.FC = () => {
-    return (
-        <nav className="bottom-nav">
-            <Link href="/dashboard" className="bottom-nav-item">
-                <Home className="bottom-nav-icon" />
-                {/* <span>Home</span> */}
-            </Link>
+  return (
+    <nav className="bottom-nav">
+      <Link href="/dashboard/feed" className="bottom-nav-item">
+        <Home className="bottom-nav-icon" />
+        {/* <span>Home</span> */}
+      </Link>
 
-            <Link href="/dashboard/search" className="bottom-nav-item">
-                <Search className="bottom-nav-icon" />
-                {/* <span>Search</span> */}
-            </Link>
+      <Link href="/dashboard/search" className="bottom-nav-item">
+        <Search className="bottom-nav-icon" />
+        {/* <span>Search</span> */}
+      </Link>
 
-            <Link href="/dashboard/messages" className="bottom-nav-item">
-                <MessageSquare className="bottom-nav-icon" />
-                {/* <span>Messages</span> */}
-            </Link>
+      <Link href="/dashboard/messages" className="bottom-nav-item">
+        <MessageSquare className="bottom-nav-icon" />
+        {/* <span>Messages</span> */}
+      </Link>
 
-            <Link href="/profile" className="bottom-nav-item">
-                <User className="bottom-nav-icon" />
-            </Link>
-        </nav>
-    );
+      <Link href="/dashboard/profile" className="bottom-nav-item">
+        <User className="bottom-nav-icon" />
+      </Link>
+    </nav>
+  );
 };
 
 export default BottomNav;
